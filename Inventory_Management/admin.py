76c24203dfde80cdb1a02e94c528634a78271b40
Mallics <hmyles1@wccnet.edu>
2013-06-17 03:31:32 -0400

    Minor tweaks to models.

--- Inventory_Management/admin.py
+++ Inventory_Management/admin.py
@@ -1,24 +1,35 @@
 from django.contrib import admin
 from Inventory_Management.models import *
 
-class EquipAdmin(admin.ModelAdmin):
+#class EquipAdmin(admin.ModelAdmin):
 	#this line changes what the information given when just eyeing the list shows
-	list_display = ('description', 'equip_type', 'model_num', 'serial_num')
+	#list_display = ('description', 'equip_type', 'model_num', 'serial_num')
 	#this does the filters on the right hand side, currently, location does not work if added
 	#list_filter = ('equip_type',)
 	#	serch currently errors when used
 	#search_fields = ['description']
 
-admin.site.register(Equipment, EquipAdmin)
+#admin.site.register(Equipment, EquipAdmin)
 #this permits adding objects of that type when adding new equipment
-admin.site.register(Equip_Type)
+#admin.site.register(Equip_Type)
+
+
+#class ComputerAdmin(admin.ModelAdmin):
+#	fieldsets = [
+#		(None, {'fields':['cpu']}),
+#	]
+
+#admin.site.register(Computer, ComputerAdmin)	
+
 admin.site.register(Location)
+admin.site.register(Manufacturer)
 admin.site.register(Ram)
 admin.site.register(HardDrive)
-admin.site.register(Mother_Board)
-admin.site.register(Optical_Drive)
-admin.site.register(Flash_Memory)
+admin.site.register(Mother_board)
+admin.site.register(Central_processing_unit)
+admin.site.register(Optical_drive)
+admin.site.register(Flash_memory)
 admin.site.register(Operating_system)
-admin.site.register(PSU)
-
+admin.site.register(Power_supply_unit)
+admin.site.register(Cisco_router)
 admin.site.register(Computer)
--- Inventory_Management/models.py
+++ Inventory_Management/models.py
@@ -5,25 +5,32 @@ from django.utils import timezone
 # Create your models here.
 
 #number is implied
-class Equip_Type(models.Model):
-        eType = models.CharField(max_length=20, unique=True)
+#class Equip_Type(models.Model):
+        #eType = models.CharField(max_length=20, unique=True)
 
+ #      def __unicode__(self):
+ #               return unicode(self.eType)
+class Manufacturer(models.Model):
+        manufacturer = models.CharField(max_length= 25)
         def __unicode__(self):
-                return unicode(self.eType)
+                return unicode(self.manufacturer)
+
 
 class Location(models.Model):
-        room = models.CharField(max_length=4)
         building = models.CharField(max_length=2)
+        room = models.CharField(max_length=4)
         def __unicode__(self):
                 return unicode(self.building + self.room)
 
+
+
 class Equipment(models.Model):
-        manufacturer = models.CharField(max_length = 30)
+        manufacturer = models.ForeignKey(Manufacturer)
         model_num = models.CharField(max_length = 35, unique = True)
         serial_num = models.CharField(max_length = 35, unique = True)
-        equip_type = models.ForeignKey(Equip_Type)
+        #equip_type = models.ForeignKey(Equip_Type)
         location = models.ForeignKey(Location)
-        description = models.TextField()
+        #description = models.TextField()
         purchaseDate = models.CharField(max_length = 10)
 
         def __unicode__(self):
@@ -39,40 +46,47 @@ class Ram(Equipment):
         voltage = models.IntegerField()
         multi_channel = models.CharField(max_length = 11)
 
+
+
+
 class HardDrive(Equipment):
         size = models.IntegerField()
+        def __unicode__(self):
+                return unicode(self.manufacturer + " " + self.size)
 
-class Mother_Board(Equipment):
-        x = 2 #filler
+class Mother_board(Equipment): pass
 
-class CPU(Equipment):
+class Central_processing_unit(Equipment):
         clock_speed = models.IntegerField()
+        def __unicode__(self):
+                return unicode(self.manufacturer +  " " + clock_speed)
 
-class Optical_Drive(Equipment):
-        x = 2 #filler
+class Optical_drive(Equipment): pass
 
-class Flash_Memory(Equipment):
-        x = 2 #filler
+class Flash_memory(Equipment): pass
 
-class Operating_system(Equipment):
-        x = 2 #filler
+class Operating_system(Equipment): 
+        name = models.CharField(max_length = 11)
+        def __unicode__(self):
+                return unicode(self.name)
 
-class PSU(Equipment):
-        x = 2 #filler
+class Power_supply_unit(Equipment): 
+        power_rating = models.CharField(max_length = 8)
 
-class Cisco_Router(Equipment):
+class Cisco_router(Equipment):
         IS_tag = models.IntegerField()
         seller = models.TextField()
         ram = models.ForeignKey(Ram)
         ram_count = models.IntegerField()
-        flash_mem = models.ForeignKey(Flash_Memory)
+        flash_mem = models.ForeignKey(Flash_memory)
         num_ports = models.IntegerField()
         service = models.TextField()
 
-class Computer(Equipment):
-        optical_drive = models.ForeignKey(Optical_Drive)
+class Computer(Equipment): 
+        cpu = models.ForeignKey(Central_processing_unit)
+        optical_drive = models.ForeignKey(Optical_drive)
+        hdd = models.ForeignKey(HardDrive)
         ram = models.ForeignKey(Ram)
-        psu = models.ForeignKey(PSU)
-        cpu = models.ForeignKey(CPU)
-        service = models.CharField(max_length = 15)
+        psu = models.ForeignKey(Power_supply_unit)
+        #service = models.CharField(max_length = 15)
 
