71ead229b4211451778e82b69e58655d9fc80870
Mallics <hmyles1@wccnet.edu>
2013-06-19 12:34:04 -0400

    Tweaks added to models.py

--- Inventory_Management/models.py
+++ Inventory_Management/models.py
@@ -2,19 +2,28 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
-# Create your models here.
 
-#number is implied
-#class Equip_Type(models.Model):
-        #eType = models.CharField(max_length=20, unique=True)
 
- #      def __unicode__(self):
- #               return unicode(self.eType)
+#Base Model
+class Equipment(models.Model):
+        manufacturer = models.ForeignKey('Manufacturer')
+        model_num = models.CharField(max_length = 35, unique = True)
+        serial_num = models.CharField(max_length = 35, unique = True)
+        purchaseDate = models.DateField()
+
+#For all in one's ... ex: A PC, Router, or Server
+class Unit(Equipment):
+        location = models.ForeignKey('Location', blank=False)
+
+#For each individual item... ex: Ram stick, CPU, or PSU
+class Component(Equipment):
+        location = models.ForeignKey('Location', blank=True)
+
+
 class Manufacturer(models.Model):
         manufacturer = models.CharField(max_length= 25)
         def __unicode__(self):
                 return unicode(self.manufacturer)
-
 
 class Location(models.Model):
         building = models.CharField(max_length=2)
@@ -23,70 +32,60 @@ class Location(models.Model):
                 return unicode(self.building + self.room)
 
 
-
-class Equipment(models.Model):
-        manufacturer = models.ForeignKey(Manufacturer)
-        model_num = models.CharField(max_length = 35, unique = True)
-        serial_num = models.CharField(max_length = 35, unique = True)
-        #equip_type = models.ForeignKey(Equip_Type)
-        location = models.ForeignKey(Location)
-        #description = models.TextField()
-        purchaseDate = models.CharField(max_length = 10)
-
-        def __unicode__(self):
-                return unicode(self.description)
-
-class Ram(Equipment):
-        clock_speed = models.CharField(max_length = 5)
-        series = models.CharField(max_length = 25)
-        _type = models.CharField(max_length = 10)
+class Ram(Component):
+        clock_speed = models.CharField(max_length = 5, blank=True)
+        series = models.CharField(max_length = 25, blank=True)
+        _type = models.CharField(max_length = 10, blank=True)
         pins = models.IntegerField()
         cas_latency = models.IntegerField()
-        timing = models.CharField(max_length = 11)
+        timing = models.CharField(max_length = 11, blank=True)
         voltage = models.IntegerField()
-        multi_channel = models.CharField(max_length = 11)
+        multi_channel = models.CharField(max_length = 11, blank=True)
 
 
 
 
-class HardDrive(Equipment):
+class HardDrive(Component):
         size = models.IntegerField()
         def __unicode__(self):
-                return unicode(self.manufacturer + " " + self.size)
+                return unicode(str(self.manufacturer) + " " + str(self.size))
 
-class Mother_board(Equipment): pass
+class Mother_board(Component): pass
 
-class Central_processing_unit(Equipment):
-        clock_speed = models.IntegerField()
+class Central_processing_unit(Component):
+        clock_speed = models.FloatField()
         def __unicode__(self):
-                return unicode(self.manufacturer +  " " + clock_speed)
+                return unicode(str(self.manufacturer) +  " " + str(self.clock_speed))
 
-class Optical_drive(Equipment): pass
+class Optical_drive(Component): pass
 
-class Flash_memory(Equipment): pass
+class Flash_memory(Component): pass
 
-class Operating_system(Equipment): 
-        name = models.CharField(max_length = 11)
+class Operating_system(Component):
+        name = models.CharField(max_length = 11, blank=True)
         def __unicode__(self):
                 return unicode(self.name)
 
-class Power_supply_unit(Equipment): 
-        power_rating = models.CharField(max_length = 8)
+class Power_supply_unit(Component):
+        power_rating = models.CharField(max_length = 8, blank=True)
 
-class Cisco_router(Equipment):
+class Cisco_router(Unit):
         IS_tag = models.IntegerField()
         seller = models.TextField()
         ram = models.ForeignKey(Ram)
-        ram_count = models.IntegerField()
         flash_mem = models.ForeignKey(Flash_memory)
         num_ports = models.IntegerField()
         service = models.TextField()
 
-class Computer(Equipment): 
+class Computer(Unit):
         cpu = models.ForeignKey(Central_processing_unit)
         optical_drive = models.ForeignKey(Optical_drive)
         hdd = models.ForeignKey(HardDrive)
         ram = models.ForeignKey(Ram)
         psu = models.ForeignKey(Power_supply_unit)
         #service = models.CharField(max_length = 15)
+
+
+
+
 
