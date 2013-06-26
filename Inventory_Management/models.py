8811d8ce809047102b0dc5650d509bb725e72d2e
Mallics <hmyles1@wccnet.edu>
2013-06-25 21:15:57 -0400

    DB is good to go for 1.0

--- Inventory_Management/models.py
+++ Inventory_Management/models.py
@@ -3,89 +3,132 @@ import datetime
 from django.utils import timezone
 
 
-
-#Base Model
+ 
 class Equipment(models.Model):
-        manufacturer = models.ForeignKey('Manufacturer')
-        model_num = models.CharField(max_length = 35, unique = True)
-        serial_num = models.CharField(max_length = 35, unique = True)
-        purchaseDate = models.DateField()
+    '''
+        Base model for Unit and Component
+    '''
+    manufacturer = models.ForeignKey('Manufacturer')
+    model_num = models.ForeignKey('ModelNumber')
+    serial_num = models.CharField(max_length = 35, unique = True)
+    purchaseDate = models.DateField()
 
-#For all in one's ... ex: A PC, Router, or Server
+
 class Unit(Equipment):
-        location = models.ForeignKey('Location', blank=False)
+    '''
+        Computer, Router, Switch, etc.
+    '''
+    location = models.ForeignKey('Location', blank=False)
 
-#For each individual item... ex: Ram stick, CPU, or PSU
+
 class Component(Equipment):
-        location = models.ForeignKey('Location', blank=True)
+    '''
+        For components such as RAM, CPU, etc.
+    '''
+    location = models.ForeignKey('Location', blank=True, null=True)
+
+
+class Location(models.Model):
+    '''
+        Docstring needed
+    '''
+    building = models.CharField(max_length=2)
+    room = models.CharField(max_length=4)
+
+    def __unicode__(self):
+        return unicode(self.building + self.room)
 
 
 class Manufacturer(models.Model):
-        manufacturer = models.CharField(max_length= 25)
-        def __unicode__(self):
-                return unicode(self.manufacturer)
+    '''
+        Docstring needed
+    '''
+    name = models.CharField(max_length=25, unique=True)
 
-class Location(models.Model):
-        building = models.CharField(max_length=2)
-        room = models.CharField(max_length=4)
-        def __unicode__(self):
-                return unicode(self.building + self.room)
+    def __unicode__(self):
+        return unicode(self.name)
 
 
-class Ram(Component):
-        clock_speed = models.CharField(max_length = 5, blank=True)
-        series = models.CharField(max_length = 25, blank=True)
-        _type = models.CharField(max_length = 10, blank=True)
-        pins = models.IntegerField()
-        cas_latency = models.IntegerField()
-        timing = models.CharField(max_length = 11, blank=True)
-        voltage = models.IntegerField()
-        multi_channel = models.CharField(max_length = 11, blank=True)
+class Vendor(models.Model):
+    '''
+        Seller
+    '''
+    name = models.CharField(max_length=30, unique=True)
+
+    def __unicode__(self):
+        return unicode(self.name)
 
 
+class ModelNumber(models.Model):
+    number = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return unicode(self.number)
+
+
+class Memory(Component):
+    CHOICES = (
+        ('RAM', 'RAM'),
+        ('Flash', 'Flash'),
+    )
+    mem_type = models.CharField(max_length=10, choices=CHOICES)
+    size_in_megs = models.IntegerField()
+    type_of_ram = models.CharField(max_length=10, blank=True, null=True)
+    def __unicode__(self):
+        return unicode(str(self.manufacturer) + " " + str(self.size_in_megs) + " " + str(self.mem_type))
+
+
+class Ram(Memory): pass
+
+
+class Flash_Memory(Memory): pass
 
 
 class HardDrive(Component):
-        size = models.IntegerField()
-        def __unicode__(self):
-                return unicode(str(self.manufacturer) + " " + str(self.size))
+    size = models.IntegerField()
+
+    def __unicode__(self):
+            return unicode(str(self.manufacturer) + " " + str(self.size))
 
 class Mother_board(Component): pass
 
 class Central_processing_unit(Component):
-        clock_speed = models.FloatField()
-        def __unicode__(self):
-                return unicode(str(self.manufacturer) +  " " + str(self.clock_speed))
+    clock_speed = models.FloatField()
+    def __unicode__(self):
+        return unicode(str(self.manufacturer) +  " " + str(self.clock_speed))
 
 class Optical_drive(Component): pass
 
-class Flash_memory(Component): pass
-
 class Operating_system(Component):
-        name = models.CharField(max_length = 11, blank=True)
-        def __unicode__(self):
-                return unicode(self.name)
+    name = models.CharField(max_length = 11, blank=True)
+
+    def __unicode__(self):
+        return unicode(self.name)
 
 class Power_supply_unit(Component):
-        power_rating = models.CharField(max_length = 8, blank=True)
+    power_rating = models.CharField(max_length = 8, blank=True)
 
-class Cisco_router(Unit):
-        IS_tag = models.IntegerField()
-        seller = models.TextField()
-        ram = models.ForeignKey(Ram)
-        flash_mem = models.ForeignKey(Flash_memory)
-        num_ports = models.IntegerField()
-        service = models.TextField()
+class Service(models.Model):
+    service = models.TextField()
 
-class Computer(Unit):
-        cpu = models.ForeignKey(Central_processing_unit)
-        optical_drive = models.ForeignKey(Optical_drive)
-        hdd = models.ForeignKey(HardDrive)
-        ram = models.ForeignKey(Ram)
-        psu = models.ForeignKey(Power_supply_unit)
-        #service = models.CharField(max_length = 15)
+class Router(Unit):
+    IS_tag = models.IntegerField()
+    ports = models.IntegerField()
+    number_of_mem_sticks = models.IntegerField()
+    service = models.ForeignKey('Service')
+    ram = models.ForeignKey('Ram')
+    flash = models.ForeignKey('Flash_Memory')
 
-
-
-
-
+    #def __unicode__(self):
+       # return unicode(self.name)
+class Computer(Unit): 
+    cpu = models.ForeignKey('Central_processing_unit')
+    optical_drive = models.ForeignKey('Optical_drive')
+    hdd = models.ForeignKey('HardDrive')
+    ram = models.ForeignKey('Ram')
+    psu = models.ForeignKey('Power_supply_unit')
+    number_of_mem_sticks = models.IntegerField()
+    IS_tag = models.IntegerField()
+        
+   # def __unicode__(self):
+      #  return unicode(self.name)
